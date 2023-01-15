2ff6820bb035447b36a41a0819ef06f11ed47eba
Forourhappy <foroh1504@gmail.com>
2023-01-16 00:39:20 +0900

    thymeleaf 경로 오류 수정

--- src/main/java/com/example/thymeleafboard/controller/FileBoardController.java
+++ src/main/java/com/example/thymeleafboard/controller/FileBoardController.java
@@ -19,12 +19,8 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
@@ -32,7 +28,6 @@ import org.springframework.web.multipart.MultipartFile;
 import com.example.thymeleafboard.bean.FileBoardVO;
 import com.example.thymeleafboard.bean.FileVO;
 import com.example.thymeleafboard.service.FileBoardService;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequestMapping("/fileBoard")
@@ -79,11 +74,12 @@ public class FileBoardController {
       String fileNameExtension = FilenameUtils.getExtension(fileName).toLowerCase();
       File destinationFile;
       String destinationFileName;
-      String fileUrl = "C:\\Users\\smhrd\\Desktop\\thymeleaf-board\\public";
+      String fileUrl = "E:\\WorkPlace\\thymeleaf-board\\public\\";
 
       do {
         destinationFileName = RandomStringUtils.randomAlphanumeric(32) + "." + fileNameExtension;
         destinationFile = new File(fileUrl + destinationFileName);
+        System.out.println(destinationFile);
       } while (destinationFile.exists());
 
       destinationFile.getParentFile().mkdirs();
@@ -122,18 +118,22 @@ public class FileBoardController {
     return "redirect:/fileBoard/list";
   }
 
-  @RequestMapping(value = "/fileDown/{b_no}")
-  public void fileDown(@PathVariable("b_no") int b_no, HttpServletRequest request, HttpServletResponse response)
-      throws UnsupportedEncodingException, Exception {
+  @RequestMapping("/fileDown/{b_no}")
+  private void fileDown(@PathVariable("b_no") int b_no, HttpServletRequest request,
+      HttpServletResponse response) throws UnsupportedEncodingException, Exception {
+
     request.setCharacterEncoding("UTF-8");
     FileVO fileVO = fBoardService.fileDetail(b_no);
-
+    System.out.println(fileVO);
+    // 파일 업로드 경로
     try {
       String fileUrl = fileVO.getFileurl();
+      System.out.println(fileUrl);
       fileUrl += "/";
       String savePath = fileUrl;
       String fileName = fileVO.getFilename();
 
+      // 실제 내보낼 파일명
       String originFileName = fileVO.getFileoriginname();
       InputStream in = null;
       OutputStream os = null;
@@ -141,6 +141,7 @@ public class FileBoardController {
       Boolean skip = false;
       String client = "";
 
+      // 파일을 읽어 스트림에 담기
       try {
         file = new File(savePath, fileName);
         in = new FileInputStream(file);
@@ -150,18 +151,20 @@ public class FileBoardController {
 
       client = request.getHeader("User-Agent");
 
+      // 파일 다운로드 헤더 지정
       response.reset();
       response.setContentType("application/octet-stream");
       response.setHeader("Content-Description", "HTML Generated Data");
-
       if (!skip) {
         // IE
         if (client.indexOf("MSIE") != -1) {
-          response.setHeader("Content-Disposition",
-              "attachment; filename=\"" + java.net.URLEncoder.encode(originFileName, "UTF-8").replace("\\+", "\\ "));
+          response.setHeader("Content-Disposition", "attachment; filename=\""
+              + java.net.URLEncoder.encode(originFileName, "UTF-8").replaceAll("\\+", "\\ ") + "\"");
+          // IE 11 이상
         } else if (client.indexOf("Trident") != -1) {
           response.setHeader("Content-Disposition", "attachment; filename=\""
               + java.net.URLEncoder.encode(originFileName, "UTF-8").replaceAll("\\+", "\\ ") + "\"");
+
           // 한글 파일명 처리
         } else {
           response.setHeader("Content-Disposition", "attachment; filename=\"" +
@@ -183,9 +186,10 @@ public class FileBoardController {
         out.println("<script> alert('파일을 찾을 수 없습니다.'); history.back(); </script>");
         out.flush();
       }
-
-      in.close();
-      os.close();
+      if (in != null && os != null) {
+        in.close();
+        os.close();
+      }
 
     } catch (Exception e) {
       System.out.println("ERROR : " + e.getStackTrace());
